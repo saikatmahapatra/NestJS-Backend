e5ff3f360fd4d1bdc469a2739890120a5c66d5ed
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-06-06 20:22:25 +0530

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('User Management Enterprise API')
+    .setDescription('CRUD API for managing application users')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // This defines the URL path
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
